089fb10e5e51a18e2099000a4ae93c0d31dc7967
Vitalii Bondalietov <bondaletov.vitaly01@gmail.com>
2020-01-24 21:24:34 +0200

    HW1

---
+++ HW1/task2.java
@@ -0,0 +1,18 @@
+
+public class task2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String name = "Vitalii";
+	String adress = "Ukraine, Lvivska oblast";
+	
+	System.out.println("What is your name?");
+	System.out.println(name);
+	System.out.println("Where are you live, " + name + "?");
+	System.out.println("I live in " + adress + ".");
+	}
+
+}
---
+++ HW1/task3.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class task3 {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+				
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter the three different standard units:");
+		
+		String a = reader.readLine();
+		String b = reader.readLine();
+		String c = reader.readLine();
+		double c1 = Double.parseDouble(a);
+		double c2 = Double.parseDouble(b);
+		double c3 = Double.parseDouble(c);
+		
+		System.out.println("Enter the three different times of call (in minutes):");
+		
+		String d = reader.readLine();
+		String e = reader.readLine();
+		String f = reader.readLine();
+		double t1 = Double.parseDouble(d);
+		double t2 = Double.parseDouble(e);
+		double t3 = Double.parseDouble(f);
+		
+		double pay1 = c1*t1;
+		double pay2 = c2*t2;
+		double pay3 = c3*t3;
+		double payAll = pay1 + pay2 + pay3;
+		
+		System.out.println("It is necessary to pay for the first call: " + pay1);
+		System.out.println("It is necessary to pay for the second call: " + pay2);
+		System.out.println("It is necessary to pay for the third call: " + pay3);
+		System.out.println("It is necessary to pay for the all calls: " + payAll);
+
+	}
+
+}
