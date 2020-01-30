8d90e8536ffcbb86e4532b409ddfdb3b78f15927
Vitalii Bondalietov <bondaletov.vitaly01@gmail.com>
2020-01-30 11:50:58 +0200

    Added Application

---
+++ HW2/Application.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Application {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				
+		Person one = new Person();
+		one.input(br);
+		one.output();
+		
+		Person two = new Person();
+		two.input(br);
+		two.output();
+		
+		Person three = new Person();
+		three.input(br);
+		three.output();
+		
+		Person four = new Person();
+		four.input(br);
+		four.output();
+		
+		Person five = new Person();
+		five.input(br);
+		five.output();
+		
+		
+		
+	}
+
+}
