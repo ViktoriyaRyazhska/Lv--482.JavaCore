544acfbe09ba97fdcff5efe983658cffe6391028
marik-420 <60194223+marik-420@users.noreply.github.com>
2020-02-03 18:45:36 +0200

    Update Main.java

--- lab3/src/lab3/Main.java
+++ lab3/src/lab3/Main.java
@@ -1,16 +1,17 @@
 package lab3;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.time.DayOfWeek;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.time.DayOfWeek;
+        import static lab3.Continent.*;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	
+
 	/*	int a =1;
 		int b=2;
 		int c=3;
@@ -20,51 +21,76 @@ public class Main {
 		}if(b %2 !=0) {
 			i++;
 		} if(c %2 !=0) {
-			i++;		
+			i++;
 	}
-		
+
 		System.out.println("i= "+i);*/
-		
+
 	/*int day;
-	
+
 	System.out.println("Vvedit day");
-	
+
 	day=Integer.parseInt(br.readLine());
-	
+
 		switch(day){
-	
+
 		case 1:
-			System.out.println("Monday , Понеділок");
+			System.out.println("Monday , пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
 			break;
 		case 2:
-			System.out.println("Вівторок,Tuesday");
+			System.out.println("ВіпїЅпїЅпїЅпїЅпїЅпїЅ,Tuesday");
 			break;
 		case 3:
-			System.out.println("Wednesday , Середа");
+			System.out.println("Wednesday , пїЅпїЅпїЅпїЅпїЅпїЅ");
 			break;
 		case 4:
-			System.out.println("Thoursday , четвер");
+			System.out.println("Thoursday , пїЅпїЅпїЅпїЅпїЅпїЅ");
 			break;
 		case 5:
-			System.out.println("Friday , Пятниця");
+			System.out.println("Friday , пїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
 			break;
 		case 6:
-			System.out.println("Saturday , Субота");
+			System.out.println("Saturday , пїЅпїЅпїЅпїЅпїЅпїЅ");
 			break;
 		case 7:
-			System.out.println("Sunday , Неділя");
+			System.out.println("Sunday , пїЅпїЅпїЅпїЅпїЅ");
 			break;
 			default:
 				System.out.println("Something Wrong!");
 				break;
 		}System.out.println(day);*/
-		continent Country;
-		C
-		switch("Country") {
-		case"Ukraine":case"Germany":case "Poland":case "Belarus":
-			Country=continent.Europe;
-		}
-	}
-}
+        String Country;
+        System.out.println("Write your country");
+Country=br.readLine();
+        switch(Country) {
+            case"Ukraine":case"Germany":case "Poland":case "Belarus":
+                System.out.println("Your continent is" + Europe );
+                System.out.println(" Your country is "+Country);
+                break;
+            case "Asia":
+                System.out.println("Your continent is" + Asia);
+               System.out.println(" Your country is Asia");
+                break;
+            case "North_America":
+                System.out.println("Your continent is" + North_Amerika);
+                System.out.println(" Your country is North_Amerika");
+                break;
+            case "South_Amerika":
+                System.out.println("Your continent is" + South_Amerika);
+                System.out.println(" Your country is South_Amerika");
+                break;
+                case "Australia":
+                System.out.println("Your continent is" + Australia);
+                System.out.println(" Your country is Australia");
+                break;
+                case "Afrika":
+                System.out.println("Your continent is" + Afrika);
+                System.out.println(" Your country is Afrika");
+                break;
+        }
 
+
+
+    }
+}
 
