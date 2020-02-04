1b6d055b196373bc790264b0e5ab19fabd7d19a2
marik-420 <60194223+marik-420@users.noreply.github.com>
2020-02-04 18:42:15 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,31 @@
-package dz3;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+package lab_5;
 
 public class Main {
+	
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		public static void main(String[] args) {
+			
+			int[] arr = {1,2,4,-3,6,5,-8,7,9,33};
+			/*- int max = arr[0];
+			int i =0;
+			while (i<arr.length) {
+				if (arr[i] > max) {
+					max = arr[i];
+			   }
+			i++;
+			}
+			System.out.println("max= "+ max);*/
+			/*- int sum =0;
+			for (int i=0;i<arr.length;i++) {
+				sum+=arr[i];}
+			System.out.println("Sum = " +sum);*/
+			
+			
 
-        Dog d1= new Dog();
-        d1.input(reader);
-        System.out.println(d1);
-    }
+			
+		
+	}
 }
+	
+
+
